a3740bcfc7575a7f81d484c9fae6d4e50cde24f6
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-25 18:03:06 +0200

    Add parser for whitespace integers
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Parser.hs
+++ src/SpaceMan/Parser.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module SpaceMan.Parser (Parser, ParserError,
-                        discardOthers, whitespaceOperator) where
+                        discardOthers,
+                        whitespaceOperator,
+                        whitespaceInteger) where
 
 import Control.Monad
 import Data.Void
@@ -9,6 +11,7 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 
 import qualified SpaceMan.Alphabet as WS
+import SpaceMan.AbstractSyntaxTree
 
 type Parser = Parsec Void String
 type ParserError = (ParseErrorBundle String Void)
@@ -25,3 +28,30 @@ whitespaceOperator :: Char -> Parser ()
 whitespaceOperator op = do
   void $ char op
   discardOthers
+
+char2bit :: Char -> IntegerBit
+char2bit c = if (c == WS.space) then Zero
+             else One
+
+bitToInteger :: IntegerBit -> Integer
+bitToInteger Zero = 0
+bitToInteger One = 1
+
+listToInteger'' :: Integer -> Int -> IntegerString -> Integer
+listToInteger'' acc _ [] = acc
+listToInteger'' acc bit (x:xs) =
+  listToInteger'' (acc + ((bitToInteger x) * 2 ^ (bit - 1))) (bit - 1) xs
+
+listToInteger' :: IntegerString -> Integer
+listToInteger' xs = listToInteger'' 0 (length xs) xs
+
+listToInteger :: IntegerString -> Integer
+listToInteger    []  = 0
+listToInteger (_:[]) = 0
+listToInteger (Zero:ns) = listToInteger'  ns
+listToInteger (One:ns) = -1 * listToInteger'  ns
+
+whitespaceInteger :: Parser Integer
+whitespaceInteger = listToInteger
+                    <$> map char2bit
+                    <$> takeWhile1P Nothing WS.fromIntegerAlphabet
