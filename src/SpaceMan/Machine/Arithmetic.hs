2e61746a612ab7cf5c1bac2de4bc44e36527fa6b
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-26 23:50:16 +0200

    Implement the rest of the arithmetic instructions
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/Arithmetic.hs
+++ src/SpaceMan/Machine/Arithmetic.hs
@@ -18,3 +18,24 @@ eval m Subtract = do
   where a = head $ tail $ stack m
         b = head $ stack m
         res = a - b
+
+eval m Multiply =
+  return m { stack = res:(drop 2 (stack m)),
+             pc = (pc m) + 1 }
+  where a = head $ tail $ stack m
+        b = head $ stack m
+        res = a * b
+
+eval m Divide =
+  return m { stack = res:(drop 2 (stack m)),
+             pc = (pc m) + 1 }
+  where a = head $ tail $ stack m
+        b = head $ stack m
+        res = a `div` b
+
+eval m Modulo =
+  return m { stack = res:(drop 2 (stack m)),
+             pc = (pc m) + 1 }
+  where a = head $ tail $ stack m
+        b = head $ stack m
+        res = a `mod` b
