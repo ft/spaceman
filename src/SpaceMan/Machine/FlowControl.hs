990a3cddc5394a82ae9ad84d6e679349554abc4f
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-27 00:19:45 +0200

    Silence unused argument warnings
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/FlowControl.hs
+++ src/SpaceMan/Machine/FlowControl.hs
@@ -10,7 +10,7 @@ eval :: WhitespaceMachine -> FlowControlOperation -> IO WhitespaceMachine
 -- The interpreter should not encounter these, because they are filtered out
 -- when loading a program into the machine; but if someone runs the interpreter
 -- on a program from another source, just ignore these.
-eval m (Tag tag) = return m
+eval m (Tag _) = return m
 
 eval m (Call tag) =
   return m { callStack = (pc m):(callStack m),
@@ -38,4 +38,4 @@ eval m Return =
              pc = address }
   where address = head (callStack m) + 1
 
-eval m ExitFromProgram = exitSuccess
+eval _ ExitFromProgram = exitSuccess
