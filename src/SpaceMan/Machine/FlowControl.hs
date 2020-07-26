30d937232d989565709c56346a64a75149ce86c6
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-26 23:47:36 +0200

    Implement the rest of the flow control operations
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/FlowControl.hs
+++ src/SpaceMan/Machine/FlowControl.hs
@@ -7,9 +7,18 @@ import SpaceMan.Machine.Types
 
 eval :: WhitespaceMachine -> FlowControlOperation -> IO WhitespaceMachine
 
+-- The interpreter should not encounter these, because they are filtered out
+-- when loading a program into the machine; but if someone runs the interpreter
+-- on a program from another source, just ignore these.
+eval m (Tag tag) = return m
+
 eval m (Call tag) =
   return m { callStack = (pc m):(callStack m),
              pc = address }
+  where address = getLabelAddress (jump m) tag
+
+eval m (Jump tag) =
+  return m { pc = address }
   where address = getLabelAddress (jump m) tag
 
 eval m (JumpIfZero tag) =
@@ -18,9 +27,11 @@ eval m (JumpIfZero tag) =
   where address = getLabelAddress (jump m) tag
         newPC = if ((head (stack m)) == 0) then address else (pc m) + 1
 
-eval m (Jump tag) =
-  return m { pc = address }
+eval m (JumpIfNegative tag) =
+  return m { stack = tail $ stack m,
+             pc = newPC }
   where address = getLabelAddress (jump m) tag
+        newPC = if ((head (stack m)) < 0) then address else (pc m) + 1
 
 eval m Return =
   return m { callStack = tail (callStack m),
