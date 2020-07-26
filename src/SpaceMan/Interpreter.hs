a7daf6d48ab16ba84a9e0367fb191e8d7640a790
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-26 16:21:19 +0200

    Move machine definition to its own module
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Interpreter.hs
+++ src/SpaceMan/Interpreter.hs
@@ -1,73 +1,11 @@
-module SpaceMan.Interpreter (loadWhitespace, run, WhitespaceMachine(..)) where
+module SpaceMan.Interpreter (loadWhitespace, run) where
 
-import Control.Concurrent
-import Control.Monad
 import Data.Char
 import Data.List
 import System.Exit
 
 import SpaceMan.AbstractSyntaxTree
-
-type JumpTable = [(String, Integer)]
-
-getLabelAddress :: JumpTable -> String -> Integer
-getLabelAddress [] _ = 0
-getLabelAddress ((tag,address):xs) t = if (tag == t)
-                                       then address
-                                       else getLabelAddress xs t
-
-type Stack = [Integer]
-type Address = Integer
-type Value = Integer
-
-type HeapDatum = (Address, Value)
-type Heap = [HeapDatum]
-
-heapStore :: Heap -> Address -> Value -> Heap
-heapStore [] a v = [ (a,v) ]
-heapStore ((addr,value):xs) a v = if (addr == a)
-                                  then (a,v):xs
-                                  else (addr,value) : heapStore xs a v
-
-heapFetch :: Heap -> Address -> Value
-heapFetch [] _ = 0
-heapFetch ((addr,value):xs) a = if (addr == a) then value
-                                               else heapFetch xs a
-
-data WhitespaceMachine = WhitespaceMachine
-  {
-    stack     :: Stack,
-    callStack :: Stack,
-    heap      :: Heap,
-    pc        :: Integer,
-    jump      :: JumpTable,
-    program   :: WhitespaceProgram
-  } deriving (Show, Eq)
-
-type StartInfo = (Integer, JumpTable, WhitespaceProgram)
-type MachineStart = Either String StartInfo
-
-extractPure :: StartInfo -> WhitespaceExpression -> MachineStart
-extractPure (idx, jt, ps) (FlowControl (Tag tag)) =
-  if tag `elem` (map fst jt)
-  then Left $ "Label " ++ tag ++ " already defined."
-  else Right (idx, jt ++ [ (tag, idx) ], ps)
-extractPure (idx, jt, ps) p = Right (idx + 1, jt, ps ++ [ p ])
-
-extractJumpTable :: WhitespaceProgram -> MachineStart
-extractJumpTable = foldM extractPure (0,[],[])
-
-loadTheMachine :: MachineStart -> Either String WhitespaceMachine
-loadTheMachine (Left msg) = Left msg
-loadTheMachine (Right (_, jumpTable, reducedProgram)) =
-  Right WhitespaceMachine {
-    stack = [],
-    callStack = [],
-    heap = [],
-    pc = 0,
-    jump = jumpTable,
-    program = reducedProgram
-  }
+import SpaceMan.Machine
 
 loadWhitespace :: WhitespaceProgram -> Either String WhitespaceMachine
 loadWhitespace = loadTheMachine . extractJumpTable
