bf476cdbd0c24855e4281c812ef768f1cf44e584
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-27 03:24:25 +0200

    Use a predicate function instead of handing a raw operator
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/FlowControl.hs
+++ src/SpaceMan/Machine/FlowControl.hs
@@ -6,21 +6,19 @@ import SpaceMan.AbstractSyntaxTree
 import SpaceMan.Machine.Tools
 import SpaceMan.Machine.Types
 
-type Comparison = (Integer -> Integer -> Bool)
+type Predicate = (Integer -> Bool)
 
-jmp :: WhitespaceMachine -> Label -> Comparison -> IO WhitespaceMachine
-jmp m tag op = return $ pcl n $ drp 1 m
+jmp :: WhitespaceMachine -> Label -> Predicate -> IO WhitespaceMachine
+jmp m tag p = return $ pcl n $ drp 1 m
   where a = lbl tag m
         [v] = peek 1 m
-        n = if (v `op` 0)
-            then a
-            else (pc m) + 1
+        n = if (p v) then a else (pc m) + 1
 
 eval :: WhitespaceMachine -> FlowControlOperation -> IO WhitespaceMachine
 eval m (Tag _)              = return $ pci m
 eval m (Call tag)           = return $ pcl a $ csp m  where a = lbl tag m
 eval m (Jump tag)           = return $ pcl a m        where a = lbl tag m
-eval m (JumpIfZero tag)     = jmp m tag (==)
-eval m (JumpIfNegative tag) = jmp m tag (<)
+eval m (JumpIfZero tag)     = jmp m tag (== 0)
+eval m (JumpIfNegative tag) = jmp m tag (<  0)
 eval m Return               = return $ pcl a $ csd m  where a = 1 + (csa m)
 eval _ ExitFromProgram      = exitSuccess
