5388855be238d5ef9d740bee65725a5675bd3b78
Frank Terbeck <ft@bewatermyfriend.org>
2020-07-27 03:36:23 +0200

    Swap has to drop items before it pushes them swapped again
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/MemoryStack.hs
+++ src/SpaceMan/Machine/MemoryStack.hs
@@ -6,6 +6,6 @@ import SpaceMan.Machine.Types
 
 eval :: WhitespaceMachine -> StackOperation -> IO WhitespaceMachine
 eval m (Push n)  = return $ pci $ psh [n] m
-eval m Duplicate = return $ pci $ psh h m       where h     = peek 1 m
-eval m Swap      = return $ pci $ psh [b,a] m   where [a,b] = peek 2 m
+eval m Duplicate = return $ pci $ psh h m               where h     = peek 1 m
+eval m Swap      = return $ pci $ psh [b,a] $ drp 2 m   where [a,b] = peek 2 m
 eval m Drop      = return $ pci $ drp 1 m
