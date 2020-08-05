fa0d59b4952a3c47f0aec9bb1f77ffe31e5b8844
Frank Terbeck <ft@bewatermyfriend.org>
2020-08-05 03:31:02 +0200

    store: Use drop early to match behaviour in fetch
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/MemoryHeap.hs
+++ src/SpaceMan/Machine/MemoryHeap.hs
@@ -5,6 +5,6 @@ import SpaceMan.Machine.Tools
 import SpaceMan.Machine.Types
 
 eval :: WhitespaceMachine -> HeapOperation -> IO WhitespaceMachine
-eval m Store = return $ pci $ drp 2 $ sto a v m  where [v,a] = peek 2 m
+eval m Store = return $ pci $ sto a v $ drp 2 m  where [v,a] = peek 2 m
 eval m Fetch = return $ pci $ psh [v] $ drp 1 m  where [a]   = peek 1 m
                                                        v     = lda a m
