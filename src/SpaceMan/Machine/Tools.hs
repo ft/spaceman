f0fa2eae8d698d6074e52636f40765e15792a4aa
Frank Terbeck <ft@bewatermyfriend.org>
2020-08-16 02:18:53 +0200

    m/tools: Make function comments into API doc-strings
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Machine/Tools.hs
+++ src/SpaceMan/Machine/Tools.hs
@@ -17,69 +17,69 @@ import SpaceMan.Machine.Types
 
 -- Stack Memory Instructions
 
--- Push: Put new elements on top of the stack.
+-- | Push: Put new elements on top of the stack.
 psh :: [Value] -> WhitespaceMachine -> WhitespaceMachine
 psh lst m = m { stack = lst ++ (stack m) }
 
--- Drop: Remove the top-most element on the stack
+-- | Drop: Remove the top-most element on the stack
 drp :: Integer -> WhitespaceMachine -> WhitespaceMachine
 drp n m = m { stack = drop (fromInteger n) $ stack m }
 
--- Peek: Read values from stack (does not modify stack)
+-- | Peek: Read values from stack (does not modify stack)
 peek :: Integer -> WhitespaceMachine -> [Value]
 peek n m = take (fromInteger n) $ stack m
 
--- Ref: Reference item on stack
+-- | Ref: Reference item on stack
 ref :: Integer -> WhitespaceMachine -> Value
 ref i m = (stack m) !! fromInteger i
 
 -- Heap Memory Instructions
 
--- Store: Put a value into a given address of the machine's heap memory
+-- | Store: Put a value into a given address of the machine's heap memory
 sto :: Address -> Value -> WhitespaceMachine -> WhitespaceMachine
 sto a v m = m { heap = heapStore (heap m) a v }
 
--- Load: Load a value from an address of the machine's heap memory and return it
+-- | Load: Load a value from an address of the machine's heap memory and return it
 lda :: Address -> WhitespaceMachine -> Value
 lda a m = heapFetch (heap m) a
 
 
 -- Call Stack Instructions
 
--- CallStackPush: Put new element onto the top of the call-stack
+-- | CallStackPush: Put new element onto the top of the call-stack
 csp :: WhitespaceMachine -> WhitespaceMachine
 csp m = m { callStack = (pc m):(callStack m) }
 
--- CallStackDrop: Remove top-most element from call-stack
+-- | CallStackDrop: Remove top-most element from call-stack
 csd :: WhitespaceMachine -> WhitespaceMachine
 csd m = m { callStack = tail (callStack m) }
 
--- CallStackAddress: Return top-most value from call-stack
+-- | CallStackAddress: Return top-most value from call-stack
 csa :: WhitespaceMachine -> Address
 csa m = head $ callStack m
 
 
 -- Process Counter Register Instructions
 
--- ProcessCounterNext: Return the next regular process counter value
+-- | ProcessCounterNext: Return the next regular process counter value
 pcn :: WhitespaceMachine -> Address
 pcn m = (pc m) + 1
 
--- ProcessCounterLoad: Load an arbitrary address into process counter
+-- | ProcessCounterLoad: Load an arbitrary address into process counter
 pcl :: Address -> WhitespaceMachine -> WhitespaceMachine
 pcl a m = m { pc = a }
 
--- ProcessCounterIncrement: Move process counter forward one address
+-- | ProcessCounterIncrement: Move process counter forward one address
 pci :: WhitespaceMachine -> WhitespaceMachine
 pci m = pcl (pcn m) m
 
--- LoadInstruction: Load instruction from program memory based on PC
+-- | LoadInstruction: Load instruction from program memory based on PC
 ldi :: WhitespaceMachine -> WhitespaceExpression
 ldi m = (program m) !! (fromInteger $ pc m)
 
 
 -- Jump Table Instructions
 
--- Label: Returns address of given label
+-- | Label: Returns address of given label
 lbl :: Label -> WhitespaceMachine -> Address
 lbl t m = getLabelAddress (jump m) t
