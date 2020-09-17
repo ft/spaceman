c9939566b25dba0fc75a4720be0221bc29b15cd2
Frank Terbeck <ft@bewatermyfriend.org>
2020-09-17 03:14:28 +0200

    Use Encoding module in Generate code
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/SpaceMan/Generate.hs
+++ src/SpaceMan/Generate.hs
@@ -5,6 +5,7 @@ import Data.Char
 
 import SpaceMan.AbstractSyntaxTree
 import qualified SpaceMan.Alphabet as WS
+import qualified SpaceMan.Encoding as EN
 
 bits :: Integer -> String -> String
 bits 0 s = s
@@ -28,45 +29,45 @@ genTag :: String -> String
 genTag t = letters t []
 
 stackGen :: StackOperation -> String
-stackGen (Push n)  = [ WS.space ]                 ++ genInt n ++ [ WS.linefeed ]
-stackGen (Copy n)  = [ WS.tabular,  WS.space    ] ++ genInt n ++ [ WS.linefeed ]
-stackGen (Slide n) = [ WS.tabular,  WS.linefeed ] ++ genInt n ++ [ WS.linefeed ]
-stackGen Duplicate = [ WS.linefeed, WS.space    ]
-stackGen Swap      = [ WS.linefeed, WS.tabular  ]
-stackGen Drop      = [ WS.linefeed, WS.linefeed ]
+stackGen (Push n)  = EN.push  ++ genInt n ++ [ WS.linefeed ]
+stackGen (Copy n)  = EN.copy  ++ genInt n ++ [ WS.linefeed ]
+stackGen (Slide n) = EN.slide ++ genInt n ++ [ WS.linefeed ]
+stackGen Duplicate = EN.duplicate
+stackGen Swap      = EN.swap
+stackGen Drop      = EN.drop
 
 heapGen :: HeapOperation -> String
-heapGen Store = [ WS.space ]
-heapGen Fetch = [ WS.tabular ]
+heapGen Store = EN.store
+heapGen Fetch = EN.fetch
 
 flowGen :: FlowControlOperation -> String
-flowGen (Tag tag)            = [ WS.space,   WS.space     ] ++ genTag tag ++ [ WS.linefeed ]
-flowGen (Call tag)           = [ WS.space,   WS.tabular   ] ++ genTag tag ++ [ WS.linefeed ]
-flowGen (Jump tag)           = [ WS.space,   WS.linefeed  ] ++ genTag tag ++ [ WS.linefeed ]
-flowGen (JumpIfZero tag)     = [ WS.tabular, WS.space     ] ++ genTag tag ++ [ WS.linefeed ]
-flowGen (JumpIfNegative tag) = [ WS.tabular, WS.tabular   ] ++ genTag tag ++ [ WS.linefeed ]
-flowGen Return               = [ WS.tabular, WS.linefeed  ]
-flowGen ExitFromProgram      = [ WS.linefeed, WS.linefeed ]
+flowGen (Tag tag)            = EN.tag        ++ genTag tag ++ [ WS.linefeed ]
+flowGen (Call tag)           = EN.call       ++ genTag tag ++ [ WS.linefeed ]
+flowGen (Jump tag)           = EN.jump       ++ genTag tag ++ [ WS.linefeed ]
+flowGen (JumpIfZero tag)     = EN.jumpIfZero ++ genTag tag ++ [ WS.linefeed ]
+flowGen (JumpIfNegative tag) = EN.jumpIfNeg  ++ genTag tag ++ [ WS.linefeed ]
+flowGen Return               = EN.callReturn
+flowGen ExitFromProgram      = EN.exit
 
 ioGen :: InputOutputOperation -> String
-ioGen PrintCharacter = [ WS.space,   WS.space   ]
-ioGen PrintNumber    = [ WS.space,   WS.tabular ]
-ioGen ReadCharacter  = [ WS.tabular, WS.space   ]
-ioGen ReadNumber     = [ WS.tabular, WS.tabular ]
+ioGen PrintCharacter = EN.printChar
+ioGen PrintNumber    = EN.printNum
+ioGen ReadCharacter  = EN.readChar
+ioGen ReadNumber     = EN.readNum
 
 mathGen :: ArithmeticOperation -> String
-mathGen Add      = [ WS.space,   WS.space    ]
-mathGen Subtract = [ WS.space,   WS.tabular  ]
-mathGen Multiply = [ WS.space,   WS.linefeed ]
-mathGen Divide   = [ WS.tabular, WS.space    ]
-mathGen Modulo   = [ WS.tabular, WS.tabular  ]
+mathGen Add      = EN.add
+mathGen Subtract = EN.substract
+mathGen Multiply = EN.multiply
+mathGen Divide   = EN.divide
+mathGen Modulo   = EN.modulo
 
 op2string :: WhitespaceExpression -> String
-op2string (StackManipulation s) = [ WS.space                ] ++ stackGen s
-op2string (HeapAccess h)        = [ WS.tabular, WS.tabular  ] ++ heapGen h
-op2string (FlowControl f)       = [ WS.linefeed             ] ++ flowGen f
-op2string (InputOutput io)      = [ WS.tabular, WS.linefeed ] ++ ioGen io
-op2string (Arithmetic a)        = [ WS.tabular, WS.space    ] ++ mathGen a
+op2string (StackManipulation s) = EN.stack       ++ stackGen s
+op2string (HeapAccess h)        = EN.heap        ++ heapGen h
+op2string (FlowControl f)       = EN.flowControl ++ flowGen f
+op2string (InputOutput io)      = EN.io          ++ ioGen io
+op2string (Arithmetic a)        = EN.artithmetic ++ mathGen a
 
 gen :: WhitespaceProgram -> String -> String
 gen [] s = s
